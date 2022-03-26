1deed3c6346a7d263e533a280fc161d1cf15221b
Kaisei <kaisei.2482480501@icloud.com>
2022-03-26 14:32:40 +0900

    [learning_log/settings.py] を編集し、モデルを有効化

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 自分のアプリケーション
+    'learning_logs',
+    # デフォルトの Django アプリケーション
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
